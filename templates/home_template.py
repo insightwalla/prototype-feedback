656b33100bdb960f24a00b8e6f4bfc211fba97fa
insightwalla <127964633+insightwalla@users.noreply.github.com>
2023-06-07 19:16:50 +0100

    key setted to other controls as well

--- templates/home_template.py
+++ templates/home_template.py
@@ -423,7 +423,7 @@ class FeedBackHelper:
          sentiment = row['Sentiment']
          options = ['POSITIVE', 'NEGATIVE', 'neutral']
          index = options.index(sentiment)
-         select_sentiment =  col1.selectbox('Sentiment', ['POSITIVE', 'NEGATIVE', 'neutral'], index =  index)
+         select_sentiment =  col1.selectbox('Sentiment', ['POSITIVE', 'NEGATIVE', 'neutral'], index = f'i_{index_to_modify}')
 
 
          select_thumbs_up = col2.checkbox('👍', value = value_for_thumbs_up, key = f't_u {index_to_modify}')
@@ -444,7 +444,7 @@ class FeedBackHelper:
             # transform the value into a string
             value = int(value)
 
-            new_value = columns_[i].number_input(label=columns_for_input[i], min_value=0, max_value=10, value=value, step=1, format=None, key=None)
+            new_value = columns_[i].number_input(label=columns_for_input[i], min_value=0, max_value=10, value=value, step=1, format=None, key=f'rate{i} - {index_to_modify}'
             # add to the list
             results.append(new_value)
 
